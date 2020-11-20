25e93161023cb0a83f8128e26cec0730eca4d10b
Wesley Ferreira <ovvesley@gmail.com>
2020-11-20 11:40:25 -0300

    Liwc: implement attr category class

---
+++ category.py
@@ -0,0 +1,10 @@
+class Category():
+    _tag = ""
+    _id = 0
+    _raw_tag = ""
+    _child_category = None
+    _parent_category = None
+    _words = None
+
+    def __init__(self):
+        pass
