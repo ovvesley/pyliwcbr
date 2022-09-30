883c94e99a668b94a1942176d4276e7de1bfc99c
Wesley <wesley.ferreira@businessmonster.com.br>
2022-09-30 13:14:37 +0000

    liwc: handle wildcard

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
    long_description = fh.read()
 setuptools.setup(
    name='pyliwcbr',
-   version='0.0.4',
+   version='0.0.5',
    author="Wesley Ferreira",
    author_email="ovvesley@protonmail.com",
    description="Biblioteca de acesso ao dicionário Liwc",
